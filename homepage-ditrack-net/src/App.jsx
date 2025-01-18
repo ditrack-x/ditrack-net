b3221a547b75f3a2e699dc4884477a010bca2eab
Ditrack <hackerherrera2@gmail.com>
2025-01-17 20:26:30 -0500

    Deploy

---
+++ homepage-ditrack-net/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+	  <p>Aquí irá una foto de perfil o un logo</p>
+      </div>
+      <h1>Bienvenido a ditrack.net</h1>
+	<h3>Esta página esta en construcción</h3>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          Dale click a este botón si estas emocionado: {count}
+        </button> 
+      </div>
+      <p className="read-the-docs">
+       Sigueme en mi red social princial en Instagram ✨ @ditrack 
+      </p>
+    </>
+  )
+}
+
+export default App
